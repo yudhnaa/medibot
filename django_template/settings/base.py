39b4a8ee90317c99bc8d421f59ca0ecabf31d068
yudhnaaaaa <hoanganhduy75@gmaill.com>
2026-01-20 14:08:21 +0700

    update settings

--- django_template/settings/base.py
+++ django_template/settings/base.py
@@ -125,6 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Custom User Model
+AUTH_USER_MODEL = "authentication.Customer"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
